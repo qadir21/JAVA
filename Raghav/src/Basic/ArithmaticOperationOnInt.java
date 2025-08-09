7f2af6cdc05c5e070dd5ef2621ea775156164af9
Abdul Qadir <qadir21@users.noreply.github.com>
2025-08-09 23:44:34 +0530

    Update

---
+++ Raghav/src/Basic/ArithmaticOperationOnInt.java
@@ -0,0 +1,33 @@
+package Basic;
+
+public class ArithmaticOperationOnInt
+{
+    public static void main(String[] args)
+    {
+       int x = 5, y = 2;
+        System.out.println(x - y);
+        System.out.println(x + y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        double a = 39;
+        double b = 10;
+        System.out.println();
+        System.out.println(a - b);
+        System.out.println(a + b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        // isko comment bolte hai
+        System.out.println("\nComment............");
+        System.out.println(69 + 1);
+        System.out.println("69 + 1");
+        System.out.println("\nArea of circle");
+        int r = 5;
+        System.out.println((3.141592 * r * r));
+        r = 9;
+        System.out.println((3.141592 * r * r));
+        System.out.println();
+        double p = 8;
+        double q = 3.141592 * p * p;
+        System.out.println(q);
+    }
+}
---
+++ Raghav/src/Basic/VeriableBasic.java
@@ -0,0 +1,26 @@
+package Basic;
+
+public class VeriableBasic
+{
+    public static void main(String[] args)
+    {
+        int x = 5;
+        System.out.print(x + " ");
+        int y;
+        y = 20;
+        System.out.println(y);
+        System.out.println(x + y);
+        x = 7;
+        System.out.println(x);
+        int a = 5;
+        System.out.println(a);
+        a = a + 7;
+        System.out.println(a);
+        a = a - 1;
+        System.out.println(a);
+        a = a * 2;
+        System.out.println(a);
+        a = a / 3;
+        System.out.println(a);
+    }
+}
